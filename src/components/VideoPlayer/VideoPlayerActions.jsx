f269a357922b584152c21eb6f89b0b0010214b52
Juan Carlos Del Rio <jcdre@hotmail.com>
2022-03-05 17:16:20 -0500

    Change Icons to paint

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,17 +1,27 @@
 import { Heart } from "../Icons/Heart";
+import { Comment } from "../Icons/Comment";
+import { Shares } from "../Icons/Shares";
 import styles from "./styles.module.css";
 
-export default function VideoPlayerActions() {
+export default function VideoPlayerActions({
+	likes = 12,
+	comments = 12,
+	shares = 12,
+	hearted = false,
+}) {
 	return (
 		<aside className={styles.actions}>
 			<div className={styles.action}>
 				<Heart />
+				<strong title="like">{likes}</strong>
 			</div>
 			<div className={styles.action}>
-				<Heart />
+				<Comment />
+				<strong title="comments">{comments}</strong>
 			</div>
 			<div className={styles.action}>
-				<Heart />
+				<Shares />
+				<strong title="share">{shares}</strong>
 			</div>
 		</aside>
 	);
